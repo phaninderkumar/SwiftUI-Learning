d77a7d2bff74e4950740fa0f22f46fa24b134d33
Phaninder <phaninder@Admins-Mac-mini.local>
2022-06-29 20:47:12 +0530

    SwiftUIAsyncImage: Added the project with different ways to use AsyncImage

---
+++ SwiftUIAsyncImage/SwiftUIAsyncImage/SwiftUIAsyncImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAsyncImageApp.swift
+//  SwiftUIAsyncImage
+//
+//  Created by Phaninder on 27/06/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAsyncImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
